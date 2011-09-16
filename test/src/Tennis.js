4b4d77aeef9162b3007a1fe886fb35764b83b9a4
Anuj Middha <anuj.middha@gmail.com>
2011-09-17 00:09:23 +0530

    scorePoint

--- test/src/Tennis.js
+++ test/src/Tennis.js
@@ -7,10 +7,26 @@ function Game(player1, player2) {
 		this.player1_score = 0;
 		this.player2_score = 0;
 	}
+	else {
+		this.player1 = false;
+		this.player2 = false;
+	}
 }
 
 Game.prototype.ready = function() {
-	this.player1 && this.player2;
+	if(this.player1 && this.player2)
+		true;
+	else
+		false;
+}
+
+Game.prototype.scorePoint = function(player) {
+	if(player == 0) {
+		this.player1_score = 15;
+	}
+	else if(player == 1) {
+		this.player2_score = 15;
+	}
 }
 
 function Player() {
