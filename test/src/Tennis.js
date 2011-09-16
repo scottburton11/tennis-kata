e58597879c0b368dc0a17c63d068f4a6944909d3
Anuj Middha <anuj.middha@gmail.com>
2011-09-16 23:51:56 +0530

    ready func

--- test/src/Tennis.js
+++ test/src/Tennis.js
@@ -4,11 +4,11 @@ function Game(player1, player2) {
 	if(player1 && player2) {
 		this.player1 = player1;
 		this.player2 = player2;
-		this.ready = true;
 	}
-	else {
-		this.ready = false;
-	}
+}
+
+Game.prototype.ready = function() {
+	this.player1 && this.player2;
 }
 
 function Player() {
