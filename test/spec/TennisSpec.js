1e2f3c1f72b82e709d5696fe7c0de1aa727dbecd
Scott Burton <scottburton11@gmail.com>
2011-09-16 11:05:13 -0700

    specify that a game must have two players to be ready

--- test/spec/TennisSpec.js
+++ test/spec/TennisSpec.js
@@ -1,1 +1,30 @@
 // Tennis specs
+
+describe("A game of Tennis", function(){
+  var game;
+  var player1;
+  var player2;
+
+  describe("with two players", function(){
+
+    beforeEach(function(){
+      player1 = new Player();
+      player2 = new Player();
+      game = new Game(player1, player2);
+    });
+
+    it("should be ready to play", function(){
+      expect(game.ready).toBeTruthy();
+    });
+  });
+
+  describe("with less than two players", function(){
+    beforeEach(function(){
+      game = new Game();
+    });
+
+    it("should not be ready to play", function(){
+      expect(game.ready).toBeFalsy();
+    });
+  });
+});
