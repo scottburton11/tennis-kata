a9549c64b2da90fbb582505351ae0c45e5249419
Anuj Middha <anuj.middha@gmail.com>
2011-09-16 23:42:34 +0530

    added check if params are not there

--- test/src/Tennis.js
+++ test/src/Tennis.js
@@ -1,7 +1,12 @@
 // Tennis
 
 function Game(player1, player2) {
-	this.player1 = player1;
-	this.player2 = player2;
-	this.ready = true;
+	if(player1 && player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+		this.ready = true;
+	}
+	else {
+		this.ready = false;
+	}
 }
